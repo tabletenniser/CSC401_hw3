88bf69c0bd608d169429be62e8b4abc887942489
yimingkang <yiming.kang@mail.utoronto.ca>
2016-04-07 21:08:35 -0700

    Test on CDF

--- IBM.m
+++ IBM.m
@@ -16,9 +16,8 @@ for file_idx = 1:n_flac
     fprintf('File name: %s -- %s\n', flac_files(file_idx).name,  text_files(file_idx).name)
     % Do some stuff
     cmd = ['env LD_LIBRARY_PATH="" curl -u 2b78670c-c890-477b-a54b-a4caeb27ad0e:JJgGDcOVXKxB -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @Testing/', flac_files(file_idx).name, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"'];
-    %fprintf('CMD = %s\n', cmd);
     [x, y] = unix(cmd);
-    fprintf('IBM result: %s\n', y);
+    %fprintf('IBM result: %s\n', y);
     confidence = regexp(y, '"confidence": (\d+\.\d+)', 'tokens');
     transcript = regexp(y, '"transcript": "([^"]+)"', 'tokens');
     confidence = confidence{1};
@@ -26,4 +25,8 @@ for file_idx = 1:n_flac
     transcript = transcript{1};
     transcript = strtrim(transcript{:});
     fprintf('Translated string: %s with confidence %s\n', transcript, confidence);
+    [xxx1, xxx2, hypothesis] = textread(['Testing/', text_files(file_idx).name], '%d %d %s', 'delimiter','\n');
+    [SE, IE, DE, LEV_DIST, N_HYP] = edit_distance(hypothesis, transcript);
+    fprintf('Edit distance between %s and %s is %d\n', hypothesis, transcript, LEV_DIST);
+
 end
