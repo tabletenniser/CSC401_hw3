fb93d989ea7f6709373656e15a7b6435cbfd0c2b
yimingkang <yiming.kang@mail.utoronto.ca>
2016-04-07 19:55:41 -0700

    Push to cdf for further testing

--- IBM.m
+++ IBM.m
@@ -1,4 +1,5 @@
 % curl -u 2b78670c-c890-477b-a54b-a4caeb27ad0e:JJgGDcOVXKxB -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @./unkn_1.flac "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"
+% [x, y] = unix('env LD_LIBRARY_PATH="" /usr/bin/curl -u 2b78670c-c890-477b-a54b-a4caeb27ad0e:JJgGDcOVXKxB -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @Testing/unkn_1.flac "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"')
 
 flac_files = dir('Testing/unkn*.flac');
 text_files = dir('Testing/unkn*.txt');
@@ -14,4 +15,13 @@ end
 for file_idx = 1:n_flac
     fprintf('File name: %s -- %s\n', flac_files(file_idx).name,  text_files(file_idx).name)
     % Do some stuff
+    cmd = ['env LD_LIBRARY_PATH="" curl -u 2b78670c-c890-477b-a54b-a4caeb27ad0e:JJgGDcOVXKxB -X POST --header "Content-Type: audio/flac" --header "Transfer-Encoding: chunked" --data-binary @Testing/', flac_files(file_idx).name, ' "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true"'];
+    fprintf('CMD = %s\n', cmd);
+    [x, y] = unix(cmd);
+    fprintf('IBM result: %s\n', y);
+    confidence = regexp(y, '"confidence": (\d+\.\d+)', 'tokens');
+    transcript = regexp(y, '"transcript": "([^"]+)"');
+    confidence = confidence{1};
+    transcript = transcript{1};
+    fprintf('Translated string: %s\n', transcript);
 end
